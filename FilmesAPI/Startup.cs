a8b6737679f06ffed7ec8d3de9b1efb6606a8685
Fabiano Mauro <fabiano.mauro@infortechms.com.br>
2022-03-16 16:10:54 -0400

    Configuração do context

--- FilmesAPI/Startup.cs
+++ FilmesAPI/Startup.cs
@@ -1,7 +1,9 @@
+using FilmesAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,7 @@ namespace FilmesAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<FilmeContext>(opts => opts.UseMySQL(Configuration.GetConnectionString("FilmeConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
