1b4bab04bf6205b1af50e728aa4d1b68125823d3
Fabiano Mauro <fabiano.mauro@infortechms.com.br>
2022-03-16 15:17:03 -0400

    Configurada as Action da controller

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -8,6 +8,7 @@ namespace FilmesAPI.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
         [Required(ErrorMessage ="O campo título é obrigatório")]
         public string Titulo { get; set; }
         [Required(ErrorMessage = "O campo diretor é obrigatório")]
@@ -16,5 +17,6 @@ namespace FilmesAPI.Models
         public string Genero { get; set; }
         [Range(1,600, ErrorMessage ="A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
+        
     }
 }
